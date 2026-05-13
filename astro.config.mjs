f87ee5f0281e56bfafbc081e6a41613c63bc5fd6
shuntaro <shuntaro.hata0731@gmail.com>
2026-05-13 10:44:14 +0900

    chore: tailwindの追加とディレクトリ整備

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcssv4 from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins[tailwindcssv4()],
+  }
+});
